7dbc0d1fcdae9d19cff7c0074193ab57fb28326f
Spyxar <spyxar@outlook.com>
2025-08-08 19:32:54 +0200

    Use primary constructor

--- TiledNet/Models/TileImageRectangle.cs
+++ TiledNet/Models/TileImageRectangle.cs
@@ -3,24 +3,15 @@
 /// <summary>
 /// A rectangle object representing the source rectangle of a tile on an image
 /// </summary>
-public class TileImageRectangle
+public class TileImageRectangle(Tileset tileset, int x, int y, int width, int height)
 {
     public static readonly TileImageRectangle Empty = new(null, 0, 0, 0, 0);
 
-    public TileImageRectangle(Tileset tileset, int x, int y, int width, int height)
-    {
-        Tileset = tileset;
-        X = x;
-        Y = y;
-        Width = width;
-        Height = height;
-    }
+    public Tileset Tileset { get; set; } = tileset;
 
-    public Tileset Tileset { get; set; }
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
 
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public int Width { get; set; }
-    public int Height { get; set; }
+    public int Width { get; set; } = width;
+    public int Height { get; set; } = height;
 }
